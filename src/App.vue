fdfbae5a2a1af09f01f38eda1517b2e5a0f02033
jswxxkf <xuekaifeng012@gmail.com>
2021-08-08 23:42:36 +0800

    fix: merge后修复未解决的冲突

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="app">
     <router-view></router-view>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { ElButton } from 'element-plus'
 
 // defineComponent就是为了配合TS做类型检查及约束，其定义了很多泛型(包括默认值)
 // 且返回 DefineComponent类型
 export default defineComponent({
   name: 'App',
-  components: { ElButton },
+  components: {},
 })
 </script>
 
